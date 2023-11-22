cbe55b882153cc77e083ba6bf2c9e92bbfeef251
Lan <2651537541@qq.com>
2023-11-22 10:35:11 +0800

    侧边栏bug

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,23 +1,59 @@
 <template>
-
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
 }
-::-webkit-scrollbar{
+
+::-webkit-scrollbar {
   width: 5px;
   height: 5px;
   position: absolute;
 
 }
-::-webkit-scrollbar-thumb{
+
+::-webkit-scrollbar-thumb {
   background: #1890ff;
 }
-::-webkit-scrollbar-track{
+
+::-webkit-scrollbar-track {
   background: #ddd;
 }
 </style>
+
+<script >
+// 解决ERROR ResizeObserver loop completed with undelivered notifications.
+
+//问题的
+
+const debounce = (fn, delay) => {
+  let timer = null;
+
+  return function () {
+    let context = this;
+
+    let args = arguments;
+
+    clearTimeout(timer);
+
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
+};
+
+// 解决ERROR ResizeObserver loop completed with undelivered notifications.
+
+const _ResizeObserver = window.ResizeObserver;
+
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+  constructor(callback) {
+    callback = debounce(callback, 16);
+
+    super(callback);
+  }
+};
+</script>
